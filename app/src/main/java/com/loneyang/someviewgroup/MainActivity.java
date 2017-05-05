a07760435af296329e957af1f7bb1da9c081223a
truehub <379205391@qq.com>
2017-05-05 14:27:26 +0800

    瀑布流简单实现_添加点击事件

--- app/src/main/java/com/loneyang/someviewgroup/MainActivity.java
+++ app/src/main/java/com/loneyang/someviewgroup/MainActivity.java
@@ -1,13 +1,48 @@
 package com.loneyang.someviewgroup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.loneyang.someviewgroup.veiw.FlowLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private FlowLayout flowLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+        flowLayout.setOnChildSelectedListener(new FlowLayout.OnChildSelectedListener() {
+            @Override
+            public void onChildSelected(String content, View child) {
+                editText.setText(content);
+            }
+        });
+    }
+
+    private void initView() {
+        editText = (EditText) findViewById(R.id.editText);
+        flowLayout = (FlowLayout) findViewById(R.id.flowLayout);
+    }
+
+    private void submit() {
+        // validate
+        String editTextString = editText.getText().toString().trim();
+        if (TextUtils.isEmpty(editTextString)) {
+            Toast.makeText(this, "吾与徐公熟美", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO validate success, do something
+
+
     }
 }
--- app/src/main/java/com/loneyang/someviewgroup/veiw/FlowLayout.java
+++ app/src/main/java/com/loneyang/someviewgroup/veiw/FlowLayout.java
@@ -4,16 +4,33 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
+import com.loneyang.someviewgroup.R;
 import java.util.ArrayList;
-
-import static android.media.CamcorderProfile.get;
 
 /**
  * Created by user on 2017/5/4.
  */
 
 public class FlowLayout extends ViewGroup {
+
+    private String childSelcted;//当前选中的child上的文字
+    private int selectedIndex = -1;//当前选中的child坐标
+
+    private OnChildSelectedListener mOnChildSelectedListener;
+
+    public void setOnChildSelectedListener(OnChildSelectedListener onChildSelectedListener) {
+        this.mOnChildSelectedListener = onChildSelectedListener;
+    }
+
+    public String getChildSelcted() {
+        return childSelcted;
+    }
+
+    public void setChildSelcted(String childSelcted) {
+        this.childSelcted = childSelcted;
+    }
+
     public FlowLayout(Context context) {
         this(context, null);
     }
@@ -67,68 +84,82 @@ public class FlowLayout extends ViewGroup {
                 (MeasureSpec.EXACTLY == heightMode) ? heightSize : height);
     }
 
-    private ArrayList<ArrayList<View>> mLines = new ArrayList<>();
-    private ArrayList<Integer> mLineHeight = new ArrayList<>();
+    private ArrayList<ArrayList<View>> mLines = new ArrayList<>();//二级List，里面有所有的view
+    private ArrayList<Integer> mLineHeight = new ArrayList<>();//记录每一行的行高
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
         mLines.clear();
         mLineHeight.clear();
-
         int width = getWidth();
-
         int lineWidth = 0, lineHeight = 0;
 
         ArrayList<View> lineViews = new ArrayList<>();
-
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
             int childWidth = child.getMeasuredWidth();
             int childHeight = child.getMeasuredHeight();
             MarginLayoutParams childParams = (MarginLayoutParams) child.getLayoutParams();
-
-            //需要换行
+            //先处理需要换行的情况
             if (lineWidth + childWidth + childParams.leftMargin + childParams.rightMargin > width) {
                 mLineHeight.add(lineHeight);
                 mLines.add(lineViews);
                 lineWidth = 0;
                 lineViews = new ArrayList<>();
             }
-            //不换行时，累加
+            //不用换行时，累加
             lineWidth = childWidth + childParams.leftMargin + childParams.rightMargin + lineWidth;
             lineHeight = Math.max(lineHeight, childParams.topMargin + childParams.bottomMargin + childHeight);
             lineViews.add(child);
+            //child的点击事件
+            final int finalI = i;
+            child.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (v.getTag() == null) v.setTag(true);
+
+                    if (selectedIndex != finalI) {//本次点击view不是上次选中的view
+                        if (selectedIndex != -1) {
+                            getChildAt(selectedIndex).setBackground(getResources().getDrawable(R.drawable.tv_bg));
+                        }
+                        v.setBackground(getResources().getDrawable(R.drawable.tv_bg_selcected));//设定选中效果
+                        selectedIndex = finalI;
+                        TextView tv = (TextView) v;
+                        childSelcted = (String) tv.getText();
+                        mOnChildSelectedListener.onChildSelected(childSelcted,v);
+                    } else {//点击的是已经选中的view，取消选中效果
+                        selectedIndex = -1;
+                        v.setBackground(getResources().getDrawable(R.drawable.tv_bg));
+                        childSelcted = null;
+                    }
+
+                }
+            });
         }
         //记录最后一行
         mLineHeight.add(lineHeight);
         mLines.add(lineViews);
 
 
-        int left = 0 , top = 0 ;
+        int left = 0, top = 0;
         //根据行数和每行的view个数遍历所有view
 
         for (int i = 0; i < mLines.size(); i++) {
-
             lineViews = mLines.get(i);
             lineHeight = mLineHeight.get(i);
-
             //遍历当前行所有的view
             for (int j = 0; j < lineViews.size(); j++) {
                 View childView = lineViews.get(j);
                 if (childView.getVisibility() == GONE) continue;
-
                 MarginLayoutParams childParams = (MarginLayoutParams) childView.getLayoutParams();
-
                 int childLeft = left + childParams.leftMargin;
                 int childTop = top + childParams.topMargin;
                 int childBottom = childTop + childView.getMeasuredHeight();
                 int childRight = childLeft + childView.getMeasuredWidth();
-                childView.layout(childLeft,childTop,childRight,childBottom);
-
+                childView.layout(childLeft, childTop, childRight, childBottom);
                 left += childView.getMeasuredWidth() + childParams.leftMargin + childParams.rightMargin;
             }
-            left = 0 ;
+            left = 0;
             top += lineHeight;
         }
     }
@@ -136,5 +167,9 @@ public class FlowLayout extends ViewGroup {
     @Override
     public LayoutParams generateLayoutParams(AttributeSet attrs) {
         return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    public interface OnChildSelectedListener{
+        void onChildSelected(String content,View child);
     }
 }
